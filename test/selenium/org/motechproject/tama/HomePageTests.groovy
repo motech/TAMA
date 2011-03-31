96064050a6208dc1848398314e932bc64f6bf828
Ricky <wlricky@hotmail.com>
2011-03-31 16:21:48 -0700

    MOTECH-341 added selenium-rc plugin and a simple selenium test

---
+++ test/selenium/org/motechproject/tama/HomePageTests.groovy
@@ -0,0 +1,22 @@
+package org.motechproject.tama
+
+import grails.plugins.selenium.*
+import org.junit.*
+import static org.junit.Assert.*
+import static org.hamcrest.Matchers.*
+
+@Mixin(SeleniumAware)
+class HomePageTests extends GroovyTestCase {
+    @Before void setUp() {
+    }
+
+    @After void tearDown() {
+        super.tearDown()
+    }
+
+    @Test void something() {
+		selenium.open "/TAMA"
+		//FIXME: check some real content when we have our own UI
+		assertTrue selenium.isTextPresent("Grails")
+    }
+}

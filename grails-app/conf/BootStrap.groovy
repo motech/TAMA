bdb5ca7e8645858bd57977a02812e843bf0da07b
Ricky <wlricky@hotmail.com>
2011-04-06 13:37:03 -0700

    MOTECH-322 patient service to handle CRUD

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,46 @@
+import org.motechproject.tama.Clinic
+import org.motechproject.tama.Doctor
+import org.motechproject.tama.dao.ClinicDao
+import org.motechproject.tama.dao.DoctorDao
+
 class BootStrap {
 
-    def init = { servletContext ->
-    }
-    def destroy = {
-    }
+	def DoctorDao tamaDoctorDao
+	def ClinicDao tamaClinicDao
+
+	def init = { servletContext ->
+		//FIXME: delete these once we have UI for managing doctor and clinic
+		String clinicId="1"
+		String doctorXId="1"
+		String doctorYId="2"
+
+		if (!tamaClinicDao.contains(clinicId)){
+			Clinic clinic = new Clinic(
+					id: clinicId,
+					name:"Test Clinic"
+					)
+			tamaClinicDao.add(clinic)
+		}
+
+		if (!tamaDoctorDao.contains(doctorXId)){
+			Doctor doctorX = new Doctor(
+					id: doctorXId,
+					clinicId:clinicId,
+					name:"Doctor X"
+					)
+			tamaDoctorDao.add(doctorX)
+		}
+		
+		if (!tamaDoctorDao.contains(doctorYId)){
+			Doctor doctorY = new Doctor(
+					id: doctorYId,
+					clinicId:clinicId,
+					name:"Doctor Y"
+					)
+			tamaDoctorDao.add(doctorY)
+		}
+	}
+
+	def destroy = {
+	}
 }
--- grails-app/services/org/motechproject/tama/PatientService.groovy
+++ grails-app/services/org/motechproject/tama/PatientService.groovy
@@ -1,14 +1,20 @@
 package org.motechproject.tama
 
-import org.motechproject.tama.dao.PatientDao
 import org.motechproject.appointmentreminder.dao.PatientDAO as ARPatientDAO
+import org.motechproject.appointmentreminder.model.Doctor as ARDoctor
 import org.motechproject.appointmentreminder.model.Patient as ARPatient
+import org.motechproject.appointmentreminder.model.Clinic as ARClinic
+import org.motechproject.tama.dao.ClinicDao
+import org.motechproject.tama.dao.DoctorDao
+import org.motechproject.tama.dao.PatientDao
 
 class PatientService {
 
     static transactional = false
 	
 	def PatientDao tamaPatientDao
+	def DoctorDao tamaDoctorDao
+	def ClinicDao tamaClinicDao
 	
 	def ARPatientDAO appointmentReminderPatientDAO
 
@@ -24,15 +30,59 @@ class PatientService {
 	def createPatient(Patient patient){
 		tamaPatientDao.add(patient)
 		
-		ARPatient arPatient = new ARPatient(
-			clinicPatientId:patient.clinicPatientId,
-			gender:patient.gender.toString()
-			)
-		appointmentReminderPatientDAO.add(arPatient)
+		Clinic clinic = tamaClinicDao.get(patient.clinicId)
+		Doctor doctor = tamaDoctorDao.get(patient.doctorId)
+		
+		createPatientInAppointmentReminder(patient, clinic, doctor)
+		
+		return patient
 	}
 	
+	private createPatientInAppointmentReminder(Patient patient, Clinic clinic, Doctor doctor){
+		ARClinic arClinic = new ARClinic(id:clinic.id, name:clinic.name)
+		ARDoctor arDoctor = new ARDoctor(id:doctor.id, name:doctor.name, clinic:arClinic)
+		
+		ARPatient arPatient = new ARPatient(
+			id:patient.id,
+			clinicPatientId:patient.clinicPatientId,
+			gender:patient.gender.toString(),
+			clinic:arClinic,
+			doctor:arDoctor,
+			phoneNumber:patient.phoneNumber
+			)
+		
+		appointmentReminderPatientDAO.add(arPatient)
+		return arPatient
+	}
+		
 	def updatePatient(Patient patient){
 		tamaPatientDao.update(patient)
 		
+		Clinic clinic = tamaClinicDao.get(patient.clinicId)
+		Doctor doctor = tamaDoctorDao.get(patient.doctorId)
+		
+		updatePatientInAppointmentReminder(patient, clinic, doctor)
+		return patient
 	}
+	
+	private updatePatientInAppointmentReminder(Patient patient, Clinic clinic, Doctor doctor){
+		ARClinic arClinic = new ARClinic(id:clinic.id, name:clinic.name)
+		ARDoctor arDoctor = new ARDoctor(id:doctor.id, name:doctor.name, clinic:arClinic)
+		
+		ARPatient arPatient = appointmentReminderPatientDAO.get(patient.id);
+		arPatient.clinicPatientId = patient.clinicPatientId
+		arPatient.gender = patient.gender.toString()
+		arPatient.clinic = arClinic
+		arPatient.doctor = arDoctor
+		arPatient.phoneNumber = arPatient.phoneNumber
+		appointmentReminderPatientDAO.update(arPatient)
+		return arPatient
+	}
+	
+	def deletePatient(Patient patient){
+		tamaPatientDao.remove(patient)
+		ARPatient arPatient = appointmentReminderPatientDAO.get(patient.id)
+		appointmentReminderPatientDAO.remove(arPatient)
+	}
+	
 }

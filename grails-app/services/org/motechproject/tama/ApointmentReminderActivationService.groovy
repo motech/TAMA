e8ce76e9d8d9c6bb3bdc35d0f94777ea2f147d8a
az44 <az44@az44-dt.(none)>
2011-04-08 11:54:33 -0700

    MOTECH-335 AppointmentReminderActivationService initial implementation

---
+++ grails-app/services/org/motechproject/tama/ApointmentReminderActivationService.groovy
@@ -0,0 +1,58 @@
+package org.motechproject.tama
+
+import org.motechproject.appointmentreminder.dao.PatientDAO as ARPatientDao
+import org.motechproject.eventgateway.EventGateway
+import org.motechproject.model.MotechEvent
+
+class ApointmentReminderActivationService {
+
+    static transactional = false
+
+    def ARPatientDao appointmentReminderPatientDao
+    def EventGateway eventGateway
+    def PatientService patientService
+
+
+
+    def schedulePatientAppointmentReminders(String patientId) {
+
+        patientService.getPatient(patientId).appointments.each {
+
+
+            String jobId = UUID.randomUUID().toString()
+            it.reminderScheduledJobId = jobId
+
+            appointmentReminderPatientDao.addAppointment(it)
+
+            //TODO set the following variables properly
+            String eventType = "???" // schedule appointment job event type - constant should be somewhere in the code
+            String patientIdKey =  "???" // constant should be somewhere in the code
+            String appointmentIdKey = "???" // constant should be somewhere in the code
+
+            eventParameters = [patientIdKey: it.patientId, appointmentIdKey: it.id];
+
+
+
+            MotechEvent motechEvent = new MotechEvent(jobId, eventType, eventParameters);
+
+            eventGateway.sendEventMessage(motechEvent)
+        }
+
+    }
+
+    def unschedulePatientAppointmentReminders(String patientId) {
+
+        appointmentReminderPatientDao.get(patientId).appointments.each {
+
+
+             //TODO set the following variables properly
+            String eventType = "???" // unschedule job event type - constant should be somewhere in the code
+
+
+            MotechEvent motechEvent = new MotechEvent(it.reminderScheduledJobId, eventType, null);
+            eventGateway.sendEventMessage(motechEvent)
+
+            appointmentReminderPatientDao.removeAppointment(it)
+        }
+    }
+}
